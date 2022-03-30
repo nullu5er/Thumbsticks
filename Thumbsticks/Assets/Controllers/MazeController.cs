cf3d5ccb47bb11cb7594c6586fe0c5e795117bb2
Jeff Simon <s219049@students.aie.edu>
2022-03-29 17:56:51 -0700

    Mazeball need gyroscope

--- Thumbsticks/Assets/Controllers/MazeController.cs
+++ Thumbsticks/Assets/Controllers/MazeController.cs
@@ -6,11 +6,11 @@ public class MazeController : MonoBehaviour
 {
     [SerializeField] GameObject mazeBoard;
     public Thumbstick thumbstick;
-
+    float rotX, rotZ;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rotX = rotZ = 0.0f;
     }
 
     // Update is called once per frame
@@ -25,40 +25,95 @@ public class MazeController : MonoBehaviour
         input.x = Input.GetAxis("Vertical");
 #endif
 
-        Vector3 rotation = new Vector3(input.x, 0.0f, input.y);
-
-        Vector3 rotationCache = mazeBoard.transform.rotation.eulerAngles;
-
-        mazeBoard.transform.Rotate(rotation);
-
-        if (Mathf.Abs(mazeBoard.transform.rotation.eulerAngles.x) > 45.0f && Mathf.Abs(mazeBoard.transform.rotation.eulerAngles.x) < 315.0f)
+        if(Mathf.Abs(input.x) > Mathf.Epsilon)
         {
             if (input.x > 0.0f)
             {
-                Vector3 rotationTemp = new Vector3(45, 0.0f, mazeBoard.transform.rotation.eulerAngles.z);
-                mazeBoard.transform.rotation = Quaternion.Euler(rotationTemp);
+                if(Mathf.Abs(mazeBoard.transform.rotation.eulerAngles.x)+input.x > 20.0f)
+                { rotX = 0.0f; }
+                else
+                { rotX = input.x; }
             }
             else if (input.x < 0.0f)
             {
-                Vector3 rotationTemp = new Vector3(315, 0.0f, mazeBoard.transform.rotation.eulerAngles.z);
-                mazeBoard.transform.rotation = Quaternion.Euler(rotationTemp);
+                if (Mathf.Abs(mazeBoard.transform.rotation.eulerAngles.x) + input.x < 340.0f)
+                { rotX = 0.0f; }
+                else
+                { rotX = input.x; }
             }
         }
 
-
-        if (Mathf.Abs(mazeBoard.transform.rotation.eulerAngles.z) > 45.0f && Mathf.Abs(mazeBoard.transform.rotation.eulerAngles.z) < 315.0f)
+        if (Mathf.Abs(input.y) > Mathf.Epsilon)
         {
             if (input.y > 0.0f)
             {
-                Vector3 rotationTemp = new Vector3(mazeBoard.transform.rotation.eulerAngles.x, 0.0f, 45.0f);
-                mazeBoard.transform.rotation = Quaternion.Euler(rotationTemp);
+                if (Mathf.Abs(mazeBoard.transform.rotation.eulerAngles.z) + input.y > 20.0f)
+                { rotZ = 0.0f; }
+                else
+                { rotZ = input.y; }
             }
-            else if(input.y < 0.0f)
+            else if (input.y < 0.0f)
             {
-                Vector3 rotationTemp = new Vector3(mazeBoard.transform.rotation.eulerAngles.x, 0.0f, 315.0f);
-                mazeBoard.transform.rotation = Quaternion.Euler(rotationTemp);
+                if (Mathf.Abs(mazeBoard.transform.rotation.eulerAngles.z) + input.y < 340.0f)
+                { rotZ = 0.0f; }
+                else
+                { rotZ = input.y; }
             }
         }
+
+        Vector3 rotationVec = new Vector3(rotX, 0.0f, rotZ);
+
+        mazeBoard.transform.Rotate(rotationVec);
+        //Vector3 rotation = new Vector3(input.x, 0.0f, input.y);
+
+        //Vector3 rotationCache = mazeBoard.transform.rotation.eulerAngles;
+
+
+        //if (Mathf.Abs(mazeBoard.transform.rotation.eulerAngles.x) > 20.0f && Mathf.Abs(mazeBoard.transform.rotation.eulerAngles.x) < 340.0f)
+        //{
+        //    if (input.x > 0.0f)
+        //    {
+        //        Vector3 rotationTemp = new Vector3(20.0f, 0.0f, mazeBoard.transform.rotation.eulerAngles.z);
+        //        mazeBoard.transform.rotation = Quaternion.Euler(rotationTemp);
+        //    }
+        //    else if (input.x < 0.0f)
+        //    {
+        //        Vector3 rotationTemp = new Vector3(340.0f, 0.0f, mazeBoard.transform.rotation.eulerAngles.z);
+        //        mazeBoard.transform.rotation = Quaternion.Euler(rotationTemp);
+        //    }            
+        //}
+        //else
+        //{
+        //    rotX += input.x;            
+        //}
+
+
+        //if (Mathf.Abs(mazeBoard.transform.rotation.eulerAngles.z) > 20.0f && Mathf.Abs(mazeBoard.transform.rotation.eulerAngles.z) < 340.0f)
+        //{
+        //    if (input.y > 0.0f)
+        //    {
+        //        Vector3 rotationTemp = new Vector3(mazeBoard.transform.rotation.eulerAngles.x, 0.0f, 20.0f);
+        //        mazeBoard.transform.rotation = Quaternion.Euler(rotationTemp);
+        //    }
+        //    else if(input.y < 0.0f)
+        //    {
+        //        Vector3 rotationTemp = new Vector3(mazeBoard.transform.rotation.eulerAngles.x, 0.0f, 340.0f);
+        //        mazeBoard.transform.rotation = Quaternion.Euler(rotationTemp);
+        //    }
+        //}
+        //else
+        //{
+        //    rotZ += input.y;
+
+        //}
+
+
+        //if (input.x > Mathf.Epsilon || input.y > Mathf.Epsilon)
+        //{
+        //    Vector3 newRotation = new Vector3(rotX, 0.0f, rotZ);
+        //    mazeBoard.transform.Rotate(newRotation);
+        //}
+
 
         //Vector3 camFwd = cam.transform.forward;
         //camFwd.y = 0;
