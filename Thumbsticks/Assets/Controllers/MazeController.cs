ace16763c2b0c9e9d9b5b99980db0c180a02ae04
Jeff Simon <s219049@students.aie.edu>
2022-03-29 17:09:57 -0700

    Need to test this out on android and PC

--- Thumbsticks/Assets/Controllers/MazeController.cs
+++ Thumbsticks/Assets/Controllers/MazeController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MazeController : MonoBehaviour
 {
     [SerializeField] GameObject mazeBoard;
+    public Thumbstick thumbstick;
 
     // Start is called before the first frame update
     void Start()
